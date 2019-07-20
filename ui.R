bd63f934c44b06e0eb48229902e998c42987292c
DominiqueCaron <dominique.caron.1@umontreal.ca>
2019-07-20 13:22:51 -0400

    Change map size

--- ui.R
+++ ui.R
@@ -56,7 +56,7 @@ fluidPage(theme=shinytheme("flatly"),
                        ),
                        mainPanel(
                      # Map to show the selected extent
-                     leafletOutput("map")
+                     leafletOutput("map", width = 1200, height = 780)
                        )
                      )
             ),
@@ -89,99 +89,3 @@ fluidPage(theme=shinytheme("flatly"),
             )
           )
 )
-          
-          
-#           
-#           
-#           
-#           
-#           
-#           
-#           sidebarLayout(
-#             sidebarPanel(
-#               h3("Download options"),
-#               # Text input for defining the maximum extent of the territory we want to study
-#               # downloading gbif's data can be time consuming, we want to get the records
-#               # only at the beginning
-#               textInput(inputId = "area", 
-#                         label = "Area of interest :"),
-#               
-#               # The user needs to choose how far from this area he wants to download the data
-#               numericInput(inputId = "max_buffer",
-#                            label = "Area to download (maximum 10 km) :",
-#                            min = 0,
-#                            max = 10,
-#                            value = 5),
-#               
-#               # What is the maximum number of records we want to download, the absolute max is
-#               # 200 000. The more we download, the longer it takes to get the data.
-#               numericInput(inputId = "max_occ",
-#                            label = "Maximum number of records :",
-#                            min = 1,
-#                            max = 200000,
-#                            value = 1000),
-#               
-#               # Action button. It will be used, when we will want to get data from gbif
-#               actionButton(inputId = "get_data",
-#                            label = "Get"),
-#               br(),
-#               h3("Filter options"),
-#               # Determine to what maximum year we want to keep the data
-#               sliderInput(inputId = "max_year",
-#                           label = "Recorded after :",
-#                           min = 1800,
-#                           max = as.integer(format(Sys.Date(), "%Y")),
-#                           value = 1800,
-#                           sep = "",
-#                           step = 1),
-#               # Determine the extent we want to explore
-#               sliderInput(inputId = "buffer",
-#                           label = "Change Extent (km) :",
-#                           min = -5,
-#                           max = 5,
-#                           value = 0,
-#                           step = 0.1),
-#               checkboxGroupInput("kingdom", "Choose kingdom :",
-#                                  c("Animals" = "Animalia",
-#                                    "Fungi" = "Fungi",
-#                                    "Plants" = "Plantae"),
-#                                  selected = c("Animalia", "Fungi", "Plantae"))
-#             ),
-#             mainPanel(
-#               tabsetPanel(
-#                 tabPanel("Raw data",
-#                          br(),
-#                          # Table for the raw data downloaded from gbif
-#                          dataTableOutput(outputId = "data")
-#                 ),
-#                 tabPanel("Species list",
-#                          br(),
-#                          # Table of the species list
-#                          dataTableOutput(outputId = "species"),
-#                          # Button to download
-#                          downloadButton("downloadspecies", "Download")
-#                 ),
-#                 tabPanel("Map",
-#                          br(),
-#                          # Map to show the selected extent
-#                          leafletOutput("map")
-#                 ),
-#                 tabPanel("Plot",
-#                          br(),
-#                          radioButtons("conserv_system", "Choose the conservation status system",
-#                                       c("Quebec (NatureServe)" = "QC_status",
-#                                         "IUCN" = "iucn"),
-#                                       inline = T),
-#                          # Plot of the distribution of conservation status
-#                          plotOutput("plot"),
-#                          br(),
-#                          h4("Information on conservation status and critera :"),
-#                          helpText(a("NatureServe conservation status definition",
-#                                     href = "http://explorer.natureserve.org/nsranks.htm")),
-#                          helpText(a("IUCN conservation status categories",
-#                                     href = "http://www.iucnredlist.org/static/categories_criteria_3_1#categories"))
-#                 )
-#               )
-#             )
-#           )
-# )
